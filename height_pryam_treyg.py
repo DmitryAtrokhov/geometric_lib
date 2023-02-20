46e3e552bc872ef21915041ae390f5fe2baa87d8
Максим Поликарпов202 <Polikarpov202example.com>
2023-02-20 14:35:41 +0300

    Добавлен файл height_pryam_treyg.py

---
+++ height_pryam_treyg.py
@@ -0,0 +1,5 @@
+import math
+a = int(input("Введите катет a:"))
+b = int(input("Введите катет b:"))
+c = math.sqrt(pow(a,2)+pow(b,2))
+h = (a*b)/c print ("Гипотенуза прямоугольного треугольника: ", h)
